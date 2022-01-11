291c0c5cb5ca49e5dce12317f9cb92b4c72a1b12
IITvamp <ayush_b@ece.iitr.ac.in>
2022-01-11 20:36:57 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import Twitter from "@material-ui/icons/Twitter";
+import { LinkedIn, GitHub } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  bottomNavContainer: {
+    background: "#222",
+  },
+  root: {
+    "& .MuiSvgIcon-root": {
+      fill: "tan",
+      "&:hover": {
+        fill: "tomato",
+        fontSize: "1.8rem",
+      },
+    },
+  },
+});
+
+const Footer = () => {
+  const classes = useStyles();
+
+  return (
+    <BottomNavigation className={classes.bottomNavContainer}>
+      <BottomNavigationAction icon={<LinkedIn />} className={classes.root} />
+      <BottomNavigationAction icon={<Twitter />} className={classes.root} />
+      <BottomNavigationAction icon={<GitHub />} className={classes.root} />
+    </BottomNavigation>
+  );
+};
+export default Footer;
