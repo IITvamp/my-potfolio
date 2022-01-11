1b48d0cbe0f7bb036cbe66ac708e3cd327e105b5
IITvamp <ayush_b@ece.iitr.ac.in>
2022-01-11 20:45:40 +0530

    Updates

---
+++ precache-manifest.5c9617f18eb1f2c0b847d2102e31575c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e034f9fbb27d6e05a571eea41b8c70bd",
+    "url": "/IITvamp/my-potfolio/index.html"
+  },
+  {
+    "revision": "6b15e5674a3130c09808",
+    "url": "/IITvamp/my-potfolio/static/css/main.dff4b653.chunk.css"
+  },
+  {
+    "revision": "cddefd3bd022dd180cc8",
+    "url": "/IITvamp/my-potfolio/static/js/2.556731b4.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/IITvamp/my-potfolio/static/js/2.556731b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b15e5674a3130c09808",
+    "url": "/IITvamp/my-potfolio/static/js/main.1000e222.chunk.js"
+  },
+  {
+    "revision": "48a1e0fd928cd1848332",
+    "url": "/IITvamp/my-potfolio/static/js/runtime-main.2b3dc622.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/IITvamp/my-potfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/IITvamp/my-potfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/IITvamp/my-potfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/IITvamp/my-potfolio/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/IITvamp/my-potfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/IITvamp/my-potfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/IITvamp/my-potfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/IITvamp/my-potfolio/static/media/react.f370c1e0.png"
+  }
+]);
